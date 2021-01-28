091a54ddb0cd7c208576bbf390dca23dc8f72e45
Abhishek Kumar <abhishekkr95.dev@gmail.com>
2021-01-29 02:57:49 +0530

    Handling Get Request

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello From server side!', app: 'Natours' });
-});
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.post('/', (_, res) => {
-  res.send('A sample post request');
+app.get('/api/v1/tours', (_, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours
+    }
+  });
 });
 
 const port = 3000;
