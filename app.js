ec34e19d639cec3fd54844510f4b3dc01f441a30
Abhishek Kumar <abhishekkr95.dev@gmail.com>
2021-01-29 03:51:45 +0530

    responding to url parameters

--- app.js
+++ app.js
@@ -19,6 +19,35 @@ app.get('/api/v1/tours', (_, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+
+  // if (id > tours.length) {
+  //   return res.status(404).json({
+  //     status: 'fail',
+  //     message: 'Invalid ID'
+  //   });
+  // }
+
+  const tour = tours.find((tour) => {
+    return tour.id === id;
+  });
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: tour
+    }
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
