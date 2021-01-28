732f0458a88be6ed1499963c321f35ffd8780dac
Abhishek Kumar <abhishekkr95.dev@gmail.com>
2021-01-29 04:16:49 +0530

    Handling Delete Request

--- app.js
+++ app.js
@@ -88,6 +88,26 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+
+  const tour = tours.find((tour) => {
+    return tour.id === id;
+  });
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
+
 const port = 3000;
 
 app.listen(port, () => {
