649af5f58c0d20d05cfc860e156e3f87a51e51dc
Abhishek Kumar <abhishekkr95.dev@gmail.com>
2021-03-14 19:23:04 +0530

    Aggregation Pipeline Node Js

--- src/controllers/tourController.js
+++ src/controllers/tourController.js
@@ -1,9 +1,25 @@
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
+
+exports.aliasTopTours = (req, _, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
 
 // Route handlers
-exports.getAllTours = async (_, res) => {
+exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // Execute Query
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
+
+    // Send Response
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -12,6 +28,7 @@ exports.getAllTours = async (_, res) => {
       }
     });
   } catch (err) {
+    console.log(err);
     res.status(404).json({ status: 'fail', message: err });
   }
 };
@@ -76,6 +93,44 @@ exports.deleteTour = async (req, res) => {
       data: null
     });
   } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
+
+exports.getTourStats = async (_, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      { $match: { ratingsAverage: { $gte: 4.6 } } },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: 'ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' }
+        }
+      },
+      {
+        $sort: {
+          avgPrice: -1
+        }
+      },
+      {
+        $match: { _id: { $ne: 'EASY' } }
+      }
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: { stats }
+    });
+  } catch (err) {
+    console.log(err);
     res.status(404).json({
       status: 'fail',
       message: err
--- src/routes/tourRoutes.js
+++ src/routes/tourRoutes.js
@@ -5,12 +5,18 @@ const {
   createTour,
   getTour,
   updateTour,
-  deleteTour
+  deleteTour,
+  aliasTopTours,
+  getTourStats
 } = require('../controllers/tourController');
 
 const router = express.Router();
 
 // router.param('id', checkID);
+
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+
+router.route('/tour-stats').get(getTourStats);
 
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
